447f947159a2ea2a4a2f85477457089053cd0df8
Kent Collins <kent.r.collins@gmail.com>
2014-11-06 22:41:46 +0000

    Update and rename Lab05.java to Lab06.java

--- Lab06.java
+++ Lab06.java
@@ -9,7 +9,7 @@
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
-public class Lab05 {
+public class Lab06 {
 
      public static void main(String[] args) {
           // TODO Your code goes here
